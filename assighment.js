be109dd1bc80f061084cf466476219b74402297c
chanakarnampoyn1 <bidseesee89@gmail.com>
2022-11-02 14:03:55 +0700

    first commit

--- assighment.js
+++ assighment.js
@@ -1,21 +1,21 @@
 let x = 20
 console.log(x)
 
-x += 10 (บวก)
+x += 10
 console.log(x) //x = x + 10; x = 20 + 10
 
-let y = 20 (ลบ)
+let y = 20
 y -= 10
 console.log(y) //y = y - 10; y = 20 - 10
 
-let a = 20 (คูณ)
+let a = 20
 a *= 10
 console.log(a) //a = a * 10; a = 20 * 10
 
-let b = 20 (หาร)
+let b = 20
 b /= 10
 console.log(a) //b = b / 10; b = 20 / 10
 
-let c = 20 (หารเอาเศษตอบ)
+let c = 20
 c %= 10
 console.log(a) //c = c % 10; c = 20 % 10
