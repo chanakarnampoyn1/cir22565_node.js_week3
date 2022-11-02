58e128b3ef9486d005bc60ea24c310e533788751
chanakarnampoyn1 <bidseesee89@gmail.com>
2022-11-02 13:41:46 +0700

    first commit

---
+++ assihgment.js
@@ -0,0 +1,21 @@
+let x = 20
+console.log(x)
+
+x += 10 (บวก)
+console.log(x) //x = x + 10; x = 20 + 10
+
+let y = 20 (ลบ)
+y -= 10
+console.log(y) //y = y - 10; y = 20 - 10
+
+let a = 20 (คูณ)
+a *= 10
+console.log(a) //a = a * 10; a = 20 * 10
+
+let b = 20 (หาร)
+b /= 10
+console.log(a) //b = b / 10; b = 20 / 10
+
+let c = 20 (หารเอาเศษตอบ)
+c %= 10
+console.log(a) //c = c % 10; c = 20 % 10
